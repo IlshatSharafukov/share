95c4b8374e7b5cfbbeb69bd2c61e2bc21152a41a
Ilshat <sharafukov@gmail.com>
2023-03-26 19:56:57 +0300

    updated readme

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,19 +1,7 @@
-resource "null_resource" "wait" {
-  provisioner "local-exec" {
-    command = "sleep 120"
-  }
-
-  depends_on = [
-    local_file.inventory
-  ]
-}
-
 resource "null_resource" "cluster" {
   provisioner "local-exec" {
     command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ansible/inventory ansible/provision.yml"
   }
 
-  depends_on = [
-    null_resource.wait
-  ]
+  depends_on = [yandex_compute_instance.yc-node, local_file.inventory]
 }
